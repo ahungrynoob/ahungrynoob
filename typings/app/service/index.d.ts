0ce1dd2ef89fd968c05f210512217ae78627f28a
dxd <dxd_sjtu@outlook.com>
2019-11-09 01:09:42 +0800

    fix: null list (#12)
    * feat: issue ipc

--- typings/app/service/index.d.ts
+++ typings/app/service/index.d.ts
@@ -1,13 +1,11 @@
 // This file is created by egg-ts-helper@1.25.6
 // Do not modify this file!!!!!!!!!
 
-import 'beidou';
-import ExportDate from '../../../app/service/date';
-import ExportIssue from '../../../app/service/issue';
+import "beidou";
+import ExportIssue from "../../../app/service/issue";
 
-declare module 'beidou' {
+declare module "beidou" {
   interface IService {
-    date: ExportDate;
     issue: ExportIssue;
   }
 }
