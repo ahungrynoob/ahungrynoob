fab174c2d6f2d56d0198c81f103d27a239bc3c26
dxd <dxd_sjtu@outlook.com>
2019-11-12 21:01:17 +0800

    feat: client list page (#19)

--- client/home/redux/reducer/index.ts
+++ client/home/redux/reducer/index.ts
@@ -5,7 +5,7 @@ import actions, { RootAction } from '../action/index';
 export const listReducer = (state: ArticleList, action: RootAction) => {
   switch (action.type) {
     case getType(actions.listUpdate):
-      return [ ...state, ...action.payload ];
+      return [ ...action.payload ];
     default:
       return state;
   }
--- client/utils/fetcher.ts
+++ client/utils/fetcher.ts
@@ -1,1 +1,41 @@
-// import fetch from 'unfetch';
+import fetch from 'unfetch';
+import { getCookie } from './index';
+import { Category } from 'typings';
+
+function getQueryString(body: object) {
+  let str = '?';
+  Object.keys(body).forEach((val) => {
+    str += val + '=' + encodeURIComponent(body[val] + '') + '&';
+  });
+  str = str.slice(0, -1);
+  return str;
+}
+
+function fetchWrapper(url: string, method: string = 'GET', body?: object) {
+  let queryString = '';
+  if (method === 'GET' && body) {
+    queryString = getQueryString(body);
+  }
+  return fetch(url + queryString, {
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-csrf-token': getCookie('csrfToken'),
+    },
+    method,
+    body: method !== 'GET' && body && JSON.stringify(body),
+  }).then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      const error = new Error(response.statusText);
+      return Promise.reject(error);
+    }
+  });
+}
+
+export const fetchArticles = (category: Category) => {
+  return fetchWrapper('/api/articles', 'GET', { category });
+};
+
+export default fetchWrapper;
